773a9263b8e521a4f1b26b65325b294d6360234e
Kate Moore <katemoore@zipcoders-MacBook-Pro-7.local>
2018-10-17 17:27:44 -0400

    Said hi to Alice and Bob

--- Main.java
+++ Main.java
@@ -1,9 +1,27 @@
+import java.util.*;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
-    public static void main(String[] args ){
 
+    public static void main(String[] args ){
+        
+        
     }
+    
+    public void getName() {
+        Scanner word = new Scanner(System.in);
+        System.out.print("What is your name?");
+        String name = word.nextLine();
+        
+            if(name.equals("Alice")) {
+            System.out.println("Hi Alice");
+        } else if (name.equals("Bob")) {
+            System.out.println("Hi Bob");
+        } else {
+            System.out.println("Hi YOU");
+        }
+        }
 }
